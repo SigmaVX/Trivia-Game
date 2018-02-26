16c5e535d09f8accfe6718d7c802f491c6053a35
SigmaVX <tonywible@gmail.com>
2018-02-26 12:09:39 -0500

    initial html layouts

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+// Display Welcome Screen - include message, counters for questions right vs. wrong, start button, instructions.  lets do a message at top with four big buttons to click on. lets go retro cartoons.
+
+// Display radom question and multiple choice answers - maybe use an object to reflect the right q & a pair while putting all objects into an array to drop in random options
+
+// have a timer start with game start that transitions to next screen if not answered in time
+
+// Let user log a guess and transition to tnext screen if answered - log if answer is right or wrong
+
+// Show transition screen after guess - have a function for transitions that shows messages for if right or wrong maybe with a variable that stores if last question was right 
+
+// initiate function that advances question - change the html with transition screen then have timer to show new information from array and reset timer
+
+// after a set number of questions end the game, show score, and ask to play again
+
+
+
