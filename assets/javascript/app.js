037f468ec5853b42e81c5801c359c9a23d6514f8
SigmaVX <tonywible@gmail.com>
2018-02-28 13:25:15 -0500

    finished draft of all JS logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,70 +13,80 @@ var questions = [
         rightAnswer: "10pm",
         wrongAnswerOne: "12pm",
         wrongAnswerTwo: "8pm",
-        wrongAnswerThree: "3pm"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
     
     questionTwo = {
         question: "Whats up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionThree = {
         question: "Wadfzsds up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionFour = {
         question: "asdfjk;as up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionFive = {
         question: "Wasdkfad;slkj;s up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionSix = {
         question: "Wasdfas up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionSeven = {
         question: "Wkjasdfs up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionEight = {
         question: "Weraeer up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionNine = {
         question: "yep up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
 
     questionTen = {
         question: "adlkj up?",
         rightAnswer: "yes",
         wrongAnswerOne: "no",
         wrongAnswerTwo: "not",
-        wrongAnswerThree: "nope"},
+        wrongAnswerThree: "3pm",
+        fact: "stuff to add"},
     ];
 
 // Stores Objects That Are Picked To Prevent Duplicate Q&A 
@@ -104,37 +114,58 @@ var transitionTime = 10;
 // Number Of Questions Asked
 var questionCount = 0;
 
-console.log(questionOne.question);
-console.log(questions[0].question);
-console.log(questions[0][1]);
+// Sets Countdown For Transition Timer
+var transInterval;
+
+// Sets Countdown For Guess Timer
+var guessInterval
+
+// Amount Of Time For A Guess
+var guessTime;
+
+// Sets The Timout Delay For A Guess
+var guessTimeout;
+
+// Sets The Timeout Delay For Transition
+var transTimeout;
 
 
-
-
-// Fuction For Picking Questions
-// math random on array
-// display that question
-
-
-
+// Function - Starts The Game
 function startGame(){
-    $("#startScreen").hide();
-    $("#questionScreen").show();
     alreadyPickedQ = [];
     correctCount = 0;
     wrongCount = 0;
-    nextQuestion();   
+    questionCount=0; 
+    clearInterval(transInterval);
+    clearInterval(guessInterval);
+    clearTimeout(guessTimeout);
+    nextQuestion();
+    $("#correctCount").text(correctCount);
+    $("#wrongCount").text(wrongCount);
+    $("#playAgain").hide();
 }
 
+// Function - Advances To Another Question
 function nextQuestion(){
+    $("#startScreen").hide();
+    $("#resultsScreen").hide();
+    $("#questionScreen").show();
     pickQuestion();
     answerShuffle();
     questionCount++;
+    console.log("Question Count: " + questionCount);
+
+    // Resets The Transition Timer So Its Ready When Called Next
+    clearInterval(transInterval);
+    transitionTime=10;
+    $("#countDown").text(transitionTime);
+
+    // Initiates Guess Timer
+    guessTimer();
 }
 
 
-
-// Randomly Picks Question Array Spot & Verifies Only Asked Once
+// Function - Randomly Picks Question Array Spot & Verifies Only Asked Once
 function pickQuestion() {
     pick = Math.floor(Math.random() * 10);
     if (alreadyPickedQ.indexOf(pick) > -1 && alreadyPickedQ.length < 10) {
@@ -154,7 +185,7 @@ function pickQuestion() {
 }
 
 
-// Randomly Shuffles Answers 
+// Function - Randomly Shuffles Answers 
 function answerShuffle() {
     answerPositions = [];
     for (var i = 0; i < 4; i++){
@@ -173,6 +204,7 @@ function answerShuffle() {
     
     console.log("Answer Positions Array: " + answerPositions);
     
+    // Assigns The Question To The HTML and Check Attribute
     $("#answerOne").text(questionSelected.rightAnswer);
     $("#answerOne").attr("check", "right");
     $("#answerTwo").text(questionSelected.wrongAnswerOne);
@@ -181,55 +213,109 @@ function answerShuffle() {
     $("#answerThree").attr("check", "wrong");
     $("#answerFour").text(questionSelected.wrongAnswerThree);
     $("#answerFour").attr("check", "wrong");
-    
 }
 
-// Check Guesses
-function checkAnswer(){
-    $("#questionScreen").hide();
-    $("#resultsScreen").show();
-    
-    // Put Check Parameter Of The Selected Div In A Var
-    userGuess = $(this).attr("check");
-    console.log("this is: " +userGuess);
 
-    if(userGuess === "right"){
-        correctCount++;
+// Function - Checks Guess
+function checkAnswer(){
+    $("#startScreen").hide();
+    $("#resultsScreen").show();
+    $("#questionScreen").hide();
+    
+    // Game End Check
+    if(questionCount === 10){
         $("#correctCount").text(correctCount);
         $("#resultsMessage").text("Correct!");
         $("#correctMessage").text("The Right Answer Was: " + questionSelected.rightAnswer);
-
-    }
-    
-    else {
-        wrongCount++;
-        $("#wrongCount").text(wrongCount);
-        $("#resultsMessage").text("Incorrect!");
-        $("#correctMessage").text("The Right Answer Was: " + questionSelected.rightAnswer);
+        $("#countDownHeader").hide();
+        $("#countDown").text("Game Over!");
+        $("#playAgain").show();
     }
 
-    teststuff();
-    
-    if(transitionTime === 0){
-        nextQuestion();
+    else{
+        // Resets The Guess Timer So Its Ready When Called Next
+        clearInterval(guessInterval);
+        clearTimeout(guessTimeout);
+        guessTime=30;
+        $("#guessTimer").text(guessTime);
+
+        // Initiates Transition Timer
+        transTimer();
+
+        // Assigns The "Check" Parameter Of The Selected Div To A Guess Var
+        userGuess = $(this).attr("check");
+        console.log("this is: " +userGuess);
+
+        // Updates Messages & Count If User Is Right Or Wrong
+        if(userGuess === "right"){
+            correctCount++;
+            $("#correctCount").text(correctCount);
+            $("#resultsMessage").text("Correct!");
+            $("#correctMessage").text("The Right Answer Was: " + questionSelected.rightAnswer);
+            $("#fact").text(questionSelected.fact);
+
+        }
+        
+        else {
+            wrongCount++;
+            $("#wrongCount").text(wrongCount);
+            $("#resultsMessage").text("Incorrect!");
+            $("#correctMessage").text("The Right Answer Was: " + questionSelected.rightAnswer);
+            $("#fact").text(questionSelected.fact);
+        }
     }
 }
 
-function teststuff(){
-    var transInterval = setInterval(function(){transTimer()}, 1000);
+
+// Function - Sets The Transition Timer When Called
+function transTimer(){
+    
+    // Clears Any Prior Intervals
+    clearInterval(transInterval);
+    transitionTime = 10;
+    $("#countDown").text(transitionTime);
+    
+    // Sets Countdown Timer Using The Nested Fuction Below
+    transInterval = setInterval(function(){transTimer()}, 1000);
+    
+    // Advances To Next Question At End Of Timer
+    transTimeout = setTimeout( nextQuestion, 10000);
 
     // Transition Screen Timer Countdown
     function transTimer(){
-        transitionTime = 100;
         transitionTime--;
         $("#countDown").text(transitionTime);
     }
 }
-         
+  
 
+// Function - Sets The Guess Timer When Called
+function guessTimer(){
+    
+    // Clears Any Prior Intervals
+    clearInterval(guessInterval);
+    clearTimeout(guessTimeout);
+    guessTime = 30;
+    $("#guessTimer").text(guessTime);
+    
+    
+    // Sets Countdown Timer Using The Nested Fuction Below
+    guessInterval = setInterval(function(){guessTimer()}, 1000);
+    
+    // Advances To Check Answer At End Of Timer
+    guessTimeout = setTimeout(checkAnswer, 30000);
+
+    // Transition Screen Timer Countdown
+    function guessTimer(){
+        guessTime--;
+        // Adds Updated Counter To Screen
+        $("#guessTimer").text(guessTime);
+    }
+}
 
 // On Click For Starting Game
 $("#playButton").on("click", startGame);
+$("#playAgain").on("click", startGame);
 
 // On Click For Guesses
 $(".answerBtn").on("click", checkAnswer);
